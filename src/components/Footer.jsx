3c6185e61a19850833c6f9f3fee872b708097857
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2024-12-21 13:11:34 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 text-center">
+      <p>© 2024 Token Tracker. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
